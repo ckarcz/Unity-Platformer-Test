7cc64dc25ca172352e7ff216efb00fdf829066a1
Chris Karcz <ckarcz@pexcard.com>
2025-03-01 16:50:21 -0500

    fix platform side collisions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -165,7 +165,7 @@ public class PlayerMovement : MonoBehaviour
     private void UpdateState()
     {
         isTouchingGround = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        isTouchingWall = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        isTouchingWall = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer) || Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, groundLayer);
 
         isCrouching = inputVerticalValue < 0;
 
@@ -184,7 +184,7 @@ public class PlayerMovement : MonoBehaviour
         isWallGrabbingLeft = isWallGrabbing && isFacingLeft;
         isWallGrabbingRight = isWallGrabbing && isFacingRight;
 
-        print($"{nameof(isTouchingGround)}: {isTouchingGround}");
+        print($"{nameof(isTouchingGround)}: {isTouchingGround}, {nameof(isTouchingWall)}: {isTouchingWall}");
     }
 
     private void UpdateAnimation()
